cbaee23e5b90d06ec0d448e41fc00dbd469131a3
Niels Hertstein <64664577+N-Hertstein@users.noreply.github.com>
2025-03-20 18:20:51 +0100

    Added robocopy command for executing the backup

--- WinBackupUtil.ps1
+++ WinBackupUtil.ps1
@@ -15,4 +15,7 @@ fuction MakeBackup($source,$destination){
     #Get number of threads for robocopy command
     #Known issue: Does not support CPU's with more than 64 logical processors (Anything with more than 32 cores and hyperthreading)
     $cputhreads = (Get-CimInstance Win32_ComputerSystem).NumberOfLogicalProcessors
+
+    #Execute backup using robocopy
+    robocopy $source "$destination/Backup" *.* /e /zb /copyall /dcopy:DATE /secfix /timfix /r:10 /w:10 /eta /log:$destination/BackupLog.log /mt:$cputhreads
 }
