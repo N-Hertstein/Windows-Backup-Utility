a8ea5df5a809b2266d7e09fdb440f8c71438e4d3
Niels Hertstein <64664577+N-Hertstein@users.noreply.github.com>
2025-03-20 18:50:54 +0100

    Create a basic cli interface to execute a backup

--- WinBackupUtil.ps1
+++ WinBackupUtil.ps1
@@ -1,5 +1,5 @@
 #Execute Backup
-fuction MakeBackup($source,$destination){
+fuction MakeBackup([string]$source, [string]$destination){
     #Verify if source exists
     if(Test-Path $source == false){
         Write-Host "Source Directory '$source' doesn't exist"
@@ -21,3 +21,12 @@ fuction MakeBackup($source,$destination){
     #Execute backup using robocopy
     robocopy $source "$destination/Backup" *.* /e /zb /copyall /dcopy:DATE /secfix /timfix /r:10 /w:10 /eta /log:$destination/BackupLog.log /mt:$cputhreads
 }
+
+#Print some text to introduce users to the script
+Write-Host "Welcome to the Windows Backup Utility! `n Follow the following instructions to get a backup started. `n"
+
+#Get source and destination from cli input
+$srcdir = Read-Host -Prompt "Enter a directory to make a backup from (Example: C:\ImportantData): "
+$destdir = Read-Host -Prompt "`n Enter a directory to make a backup to (Example: D:\FolderForBackups): "
+
+MakeBackup -source $srcdir -destination $destdir
